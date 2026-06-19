2d78ae1b0a7115552998a7d34b618385fba70949
Snowy-0101 <TTC2350SD0005@edu.tech.ac.jp>
2026-06-19 18:32:25 +0900

    Add files via upload

---
+++ ecsite/script.js
@@ -0,0 +1,13 @@
+Vue.createApp({
+    data() {
+        return {
+            name: "チョコレートケーキ",
+            price: 4800,
+            description: "チョコレートシロップとコーヒー味のホイップクリーム",
+            image: "Chocolate-coma-cake-scaled.jpg",
+            url: "https://example.com/",
+            stock: 5,
+            priceColor: "#e53935"
+        };
+    }
+}).mount("#app") 
